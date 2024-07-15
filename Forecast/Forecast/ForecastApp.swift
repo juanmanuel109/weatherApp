9212625d35614b19b9735e48b251e93d50f73a3d
juanmanuel109 <juanmanuelordonez02@icloud.com>
2024-07-15 17:55:13 -0500

    start july 15

---
+++ Forecast/Forecast/ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastApp.swift
+//  Forecast
+//
+//  Created by Juan Manuel Ordoñez on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
